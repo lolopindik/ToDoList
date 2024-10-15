11afea9a463e41356b4cc74ef222d6d1dddbc60c
Dima <dimaluka2014@gmail.com>
2024-10-16 02:02:28 +0300

    animation added

---
+++ lib/presentation/animations/transition_animation.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class TransitionAnimation {
+  static Route createRoute(Widget page) {
+    return PageRouteBuilder(
+      pageBuilder: (context, animation, secondaryAnimation) => page,
+      transitionsBuilder: (context, animation, secondaryAnimation, child) {
+        const begin = Offset(0.0, 1.0); //* Начало анимации
+        const end = Offset.zero; //* Конечная точка
+        const curve = Curves.ease;
+
+        var tween =
+            Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
+        var offsetAnimation = animation.drive(tween);
+
+        return SlideTransition(
+          position: offsetAnimation,
+          child: child,
+        );
+      },
+    );
+  }
+}
