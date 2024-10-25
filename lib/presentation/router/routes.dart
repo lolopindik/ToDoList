65c19451ea97aafddeaf4a6a064a4473188c5edd
lolopindik <dimaluka2014@gmail.com>
2024-10-25 14:41:03 +0300

    router updated

--- lib/presentation/router/routes.dart
+++ lib/presentation/router/routes.dart
@@ -1,8 +1,10 @@
 import 'package:bloc_to_do/logic/bloc/CategoryPicker/categorypicker_cubit.dart';
+import 'package:bloc_to_do/logic/bloc/ComapeData/comapre_data_bloc.dart';
 import 'package:bloc_to_do/logic/bloc/DataCollection/data_collection_bloc.dart';
 import 'package:bloc_to_do/logic/bloc/DatePicker/datepicker_bloc.dart';
 import 'package:bloc_to_do/logic/bloc/TextFieldHandler/text_field_handler_bloc.dart';
 import 'package:bloc_to_do/logic/bloc/TimePicker/timepicker_bloc.dart';
+import 'package:bloc_to_do/logic/funcs/get_json.dart';
 import 'package:bloc_to_do/presentation/animations/transition_animation.dart';
 import 'package:bloc_to_do/presentation/screens/home_screen.dart';
 import 'package:bloc_to_do/presentation/screens/task_screen.dart';
@@ -13,7 +15,11 @@ class AppRouter {
   Route onGenerateRoute(RouteSettings routeSettings) {
     switch (routeSettings.name) {
       case '/home':
-        return MaterialPageRoute(builder: (context) => const HomeScreen());
+        return MaterialPageRoute(
+            builder: (context) => BlocProvider(
+                  create: (context) => ComapreDataBloc(GetJson())..fetchData(),
+                  child: const HomeScreen(),
+                ));
       case '/add_task':
         return TransitionAnimation.createRoute(MultiBlocProvider(
           providers: [
