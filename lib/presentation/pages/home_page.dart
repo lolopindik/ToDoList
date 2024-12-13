fc91a41f9dc7e485fad37117b63ecef2bceec267
Dima <dimaluka2014@gmail.com>
2024-12-14 00:25:16 +0300

    Refactor HomePage to integrate SharedPreferences for task management. Updated layout to use FutureBuilder for asynchronous data loading, enhancing user experience by conditionally displaying tasks. Improved UI structure with a Stack layout for better visual hierarchy and responsiveness.

--- lib/presentation/pages/home_page.dart
+++ lib/presentation/pages/home_page.dart
@@ -3,76 +3,91 @@ import 'package:bloc_to_do/presentation/animations/fade_animation.dart';
 import 'package:bloc_to_do/presentation/widgets/home_lists_widgets.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:intl/intl.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class HomePage {
   Widget buildHomePage(BuildContext context) {
-    return Stack(
-      children: [
-        Column(
-          children: [
-            Flexible(
-              flex: 1,
-              child: Container(
-                decoration: const BoxDecoration(
-                  image: DecorationImage(
-                    image: AssetImage('lib/assets/img/HomeFrame.png'),
-                    fit: BoxFit.cover,
-                  ),
-                ),
-              ),
-            ),
-            Flexible(
-              flex: 3,
-              child: Container(
-                color: ToDoColors.mainColor,
-              ),
-            ),
-          ],
-        ),
-        Positioned.fill(
-          child: Padding(
-            padding: EdgeInsets.only(
-              top: MediaQuery.of(context).size.height * 0.075,
-              left: 15,
-              right: 15,
-            ),
-            child: Column(
+    return FutureBuilder<SharedPreferences>(
+        future: SharedPreferences.getInstance(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            final tasks =
+                snapshot.data!.getStringList('collectedDataList') ?? [];
+            final hasTasks = tasks.isNotEmpty;
+
+            return Stack(
               children: [
-                FadeAnimation().createAnimation(
-                  context,
-                  Text(
-                    DateFormat('MMMM d, yyy').format(DateTime.now()),
-                    style: ToDoTextStyles.white16,
-                  ),
+                Column(
+                  children: [
+                    Flexible(
+                      flex: 1,
+                      child: Container(
+                        decoration: const BoxDecoration(
+                          image: DecorationImage(
+                            image: AssetImage('lib/assets/img/HomeFrame.png'),
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      ),
+                    ),
+                    Flexible(
+                      flex: 3,
+                      child: Container(
+                        color: ToDoColors.mainColor,
+                      ),
+                    ),
+                  ],
                 ),
-                SizedBox(height: MediaQuery.of(context).size.height * 0.025),
-                FadeAnimation().createAnimation(
-                  context,
-                  const Text(
-                    'Bloc ToDo List',
-                    style: ToDoTextStyles.white30,
-                  ),
-                ),
-                SizedBox(
-                  height: MediaQuery.of(context).size.height * 0.02,
-                ),
-                ListBuilder().buildTopList(context),
-                const Align(
-                  alignment: Alignment.topLeft,
+                Positioned.fill(
                   child: Padding(
-                    padding: EdgeInsets.symmetric(vertical: 24),
-                    child: Text(
-                      'Completed',
-                      style: ToDoTextStyles.black16,
+                    padding: EdgeInsets.only(
+                      top: MediaQuery.of(context).size.height * 0.075,
+                      left: 15,
+                      right: 15,
+                    ),
+                    child: Column(
+                      children: [
+                        FadeAnimation().createAnimation(
+                          context,
+                          Text(
+                            DateFormat('MMMM d, yyy').format(DateTime.now()),
+                            style: ToDoTextStyles.white16,
+                          ),
+                        ),
+                        SizedBox(
+                            height: MediaQuery.of(context).size.height * 0.025),
+                        FadeAnimation().createAnimation(
+                          context,
+                          const Text(
+                            'Bloc ToDo List',
+                            style: ToDoTextStyles.white30,
+                          ),
+                        ),
+                        SizedBox(
+                          height: MediaQuery.of(context).size.height * 0.02,
+                        ),
+                        ListBuilder().buildTopList(context),
+                        if (hasTasks) ...[
+                          const Align(
+                            alignment: Alignment.topLeft,
+                            child: Padding(
+                              padding: EdgeInsets.symmetric(vertical: 24),
+                              child: Text(
+                                'Completed',
+                                style: ToDoTextStyles.black16,
+                              ),
+                            ),
+                          ),
+                          ListBuilder().buldBottomList(context),
+                        ],
+                      ],
                     ),
                   ),
                 ),
-                ListBuilder().buldBottomList(context),
               ],
-            ),
-          ),
-        ),
-      ],
-    );
+            );
+          }
+          return const Center(child: CupertinoActivityIndicator(radius: 20.0));
+        });
   }
 }
