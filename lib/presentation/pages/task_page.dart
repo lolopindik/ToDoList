025c95b0c309c615f63431450ebd8312e3b76af4
Dima <dimaluka2014@gmail.com>
2024-12-13 23:36:13 +0300

    Refactor TaskPage layout to enhance user interaction and maintainability. Replaced SingleChildScrollView with GestureDetector for better keyboard dismissal, and optimized widget structure for clarity. Updated padding and alignment for improved UI consistency.

--- lib/presentation/pages/task_page.dart
+++ lib/presentation/pages/task_page.dart
@@ -10,149 +10,154 @@ class TaskPage {
   Widget buildTaskPage(BuildContext context, bool edit, [Map? task]) {
     return SafeArea(
       top: false,
-      child: SingleChildScrollView(
-        keyboardDismissBehavior: ScrollViewKeyboardDismissBehavior.onDrag,
-        padding: EdgeInsets.only(
-          bottom: MediaQuery.of(context).viewInsets.bottom,
-        ),
-        child: ConstrainedBox(
-          constraints: BoxConstraints(
-            minHeight: MediaQuery.of(context).size.height * 0.9,
+      child: GestureDetector(
+        onTap: () => FocusScope.of(context).unfocus(),
+        child: SingleChildScrollView(
+          padding: EdgeInsets.only(
+            bottom: MediaQuery.of(context).viewInsets.bottom,
           ),
-          child: IntrinsicHeight(
-            child: Stack(
-              children: [
-                Column(
-                  children: [
-                    Expanded(
-                      child: Container(
-                        decoration: const BoxDecoration(
-                          image: DecorationImage(
-                            image: AssetImage('lib/assets/img/HomeFrame.png'),
-                            fit: BoxFit.cover,
-                          ),
-                        ),
+          child: ConstrainedBox(
+            constraints: BoxConstraints(
+              minHeight: MediaQuery.of(context).size.height * 0.9,
+            ),
+            child: IntrinsicHeight(
+              child: Stack(
+                children: [
+                  Column(
+                    children: [
+                      Expanded(
                         child: Container(
-                          margin: EdgeInsets.only(
-                              top: MediaQuery.of(context).size.height * 0.045),
-                          width: MediaQuery.of(context).size.width,
-                          child: Center(
-                            child: ScaleAnimation().createAnimation(
-                              context,
-                              Text(
-                                edit ? 'Edit Task' : 'Add new task',
-                                style: ToDoTextStyles.white24,
+                          decoration: const BoxDecoration(
+                            image: DecorationImage(
+                              image: AssetImage('lib/assets/img/HomeFrame.png'),
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                          child: Container(
+                            margin: EdgeInsets.only(
+                                top:
+                                    MediaQuery.of(context).size.height * 0.045),
+                            width: MediaQuery.of(context).size.width,
+                            child: Center(
+                              child: ScaleAnimation().createAnimation(
+                                context,
+                                Text(
+                                  edit ? 'Edit Task' : 'Add new task',
+                                  style: ToDoTextStyles.white24,
+                                ),
                               ),
                             ),
                           ),
                         ),
                       ),
-                    ),
-                    Expanded(
-                      flex: 5,
-                      child: Container(
-                        color: ToDoColors.mainColor,
+                      Expanded(
+                        flex: 5,
+                        child: Container(
+                          color: ToDoColors.mainColor,
+                        ),
                       ),
-                    ),
-                  ],
-                ),
-                Positioned.fill(
-                  child: Padding(
-                    padding: EdgeInsets.only(
-                      top: MediaQuery.of(context).size.height * 0.06,
-                      left: 15,
-                      right: 15,
-                    ),
-                    child: Column(
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        Align(
-                          alignment: Alignment.centerLeft,
-                          child: CircleAvatar(
-                            backgroundColor: ToDoColors.mainColor,
-                            radius: MediaQuery.of(context).size.height * 0.035,
-                            child: IconButton(
-                              onPressed: () => Navigator.pop(context),
-                              icon: Icon(Icons.close_rounded,
-                                  size: MediaQuery.of(context).size.height *
-                                      0.05),
+                    ],
+                  ),
+                  Positioned.fill(
+                    child: Padding(
+                      padding: EdgeInsets.only(
+                        top: MediaQuery.of(context).size.height * 0.06,
+                        left: 15,
+                        right: 15,
+                      ),
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          Align(
+                            alignment: Alignment.centerLeft,
+                            child: CircleAvatar(
+                              backgroundColor: ToDoColors.mainColor,
+                              radius:
+                                  MediaQuery.of(context).size.height * 0.035,
+                              child: IconButton(
+                                onPressed: () => Navigator.pop(context),
+                                icon: Icon(Icons.close_rounded,
+                                    size: MediaQuery.of(context).size.height *
+                                        0.05),
+                              ),
                             ),
                           ),
-                        ),
-                        SizedBox(
-                            height: MediaQuery.of(context).size.height * 0.052),
-                        const Align(
-                          alignment: Alignment.centerLeft,
-                          child:
-                              Text('Task Title', style: ToDoTextStyles.black16),
-                        ),
-                        TextfiledHandlerWidget(
-                                initialTitle: edit ? '${task?['title']}' : '')
-                            .buildTextHandler(context, 'title'),
-                        Padding(
-                            padding: const EdgeInsets.only(bottom: 24),
-                            child: edit
-                                ? CategoryPickerWidget(
-                                        initialCategory: task?['category'])
-                                    .buildCategoryPicker(context)
-                                : CategoryPickerWidget()
-                                    .buildCategoryPicker(context)),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              mainAxisSize: MainAxisSize.min,
-                              children: [
-                                const Align(
-                                  alignment: Alignment.centerLeft,
-                                  child: Text('Date',
-                                      style: ToDoTextStyles.black16),
-                                ),
-                                SizedBox(
-                                  width:
-                                      MediaQuery.of(context).size.width * 0.45,
-                                  child: DatePickerWidget()
-                                      .buildDatePicker(context),
-                                ),
-                              ],
-                            ),
-                            Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              mainAxisSize: MainAxisSize.min,
-                              children: [
-                                const Align(
-                                  alignment: Alignment.centerLeft,
-                                  child: Text('Time',
-                                      style: ToDoTextStyles.black16),
-                                ),
-                                SizedBox(
+                          SizedBox(
+                              height:
+                                  MediaQuery.of(context).size.height * 0.052),
+                          const Align(
+                            alignment: Alignment.centerLeft,
+                            child: Text('Task Title',
+                                style: ToDoTextStyles.black16),
+                          ),
+                          TextfiledHandlerWidget(
+                                  initialTitle: edit ? '${task?['title']}' : '')
+                              .buildTextHandler(context, 'title'),
+                          Padding(
+                              padding: const EdgeInsets.only(bottom: 24),
+                              child: edit
+                                  ? CategoryPickerWidget(
+                                          initialCategory: task?['category'])
+                                      .buildCategoryPicker(context)
+                                  : CategoryPickerWidget()
+                                      .buildCategoryPicker(context)),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                mainAxisSize: MainAxisSize.min,
+                                children: [
+                                  const Align(
+                                    alignment: Alignment.centerLeft,
+                                    child: Text('Date',
+                                        style: ToDoTextStyles.black16),
+                                  ),
+                                  SizedBox(
                                     width: MediaQuery.of(context).size.width *
                                         0.45,
-                                    child: TimePickerWidget()
-                                        .buildTimePicker(context)),
-                              ],
-                            ),
-                          ],
-                        ),
-                        const Padding(
-                          padding: EdgeInsets.only(top: 24),
-                          child: Align(
-                            alignment: Alignment.centerLeft,
-                            child: Text(
-                              'Notes',
-                              style: ToDoTextStyles.black16,
+                                    child: DatePickerWidget()
+                                        .buildDatePicker(context),
+                                  ),
+                                ],
+                              ),
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                mainAxisSize: MainAxisSize.min,
+                                children: [
+                                  const Align(
+                                    alignment: Alignment.centerLeft,
+                                    child: Text('Time',
+                                        style: ToDoTextStyles.black16),
+                                  ),
+                                  SizedBox(
+                                      width: MediaQuery.of(context).size.width *
+                                          0.45,
+                                      child: TimePickerWidget()
+                                          .buildTimePicker(context)),
+                                ],
+                              ),
+                            ],
+                          ),
+                          const Padding(
+                            padding: EdgeInsets.only(top: 24),
+                            child: Align(
+                              alignment: Alignment.centerLeft,
+                              child: Text(
+                                'Notes',
+                                style: ToDoTextStyles.black16,
+                              ),
                             ),
                           ),
-                        ),
-                        TextfiledHandlerWidget(
-                                initialNotes: edit ? '${task?['notes']}' : '')
-                            .buildTextHandler(context, 'notes')
-                      ],
+                          TextfiledHandlerWidget(
+                                  initialNotes: edit ? '${task?['notes']}' : '')
+                              .buildTextHandler(context, 'notes')
+                        ],
+                      ),
                     ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
         ),
